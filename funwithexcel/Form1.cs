39b5133a3511dd36e95895b13f2a2fdd906f14ad
AJ Otten <AOtten@ilaniresort.com>
2018-12-30 13:09:38 -0800

    Add project files.

---
+++ funwithexcel/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace funwithexcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel excel = new Excel(@"C:\Users\320051\Desktop\Copy_EAM_WIP_v7.xlsx", 15);
+            MessageBox.Show("Rows: " + excel.LastRow().ToString() +
+                            "\nrReadCell Test: " + excel.ReadCell(5,1));
+
+
+            MessageBox.Show(excel.FindValue(Input.Text));
+        }
+    }
+}
---
+++ funwithexcel/excel.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace funwithexcel
+{
+    class Excel
+    {
+        string path = "";
+        Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+        Range range;
+        Range valueFound;
+        Range rowSelect;
+
+        public Excel(string path, int Sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path, ReadOnly:true);
+            ws = wb.Worksheets[Sheet];
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            double value = ws.Cells[i, j].Value2;
+            return value.ToString();
+        }
+
+        public string FindValue(string value)
+        {
+            SetRange(1, 1, 1, LastRow());
+            valueFound = ws.Cells.Find(value);
+            return ValueRowandCol();
+        }
+
+        public int LastRow()
+        {
+            int lastRow = ws.Cells.SpecialCells(_Excel.XlCellType.xlCellTypeLastCell).Row;
+            return lastRow;
+        }
+
+        public int LastCol()
+        {
+            int lastCol = ws.Cells.SpecialCells(_Excel.XlCellType.xlCellTypeLastCell).Column;
+            return lastCol;
+        }
+
+        private string ValueRowandCol()
+        {
+            string row = valueFound.Row.ToString();
+            string col = valueFound.Column.ToString();
+
+            return "Row " + row + " Col " + col;
+        }
+
+        public void SetRange(int col1, int row1, int col2, int row2)
+        {
+            range = ws.Range[ws.Cells[col1, row1], ws.Cells[col2, row2]];
+        }
+
+        public void SelectRow(int row)
+        {
+            rowSelect = ws.Range[ws.Cells[1, row], ws.Cells[LastCol(), row]];
+        }
+
+    }
+}
